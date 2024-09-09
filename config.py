8dff0991198f50a24910d955e1d0530f65f54c7e
riaz <riaz.2012@gmail.com>
2024-09-08 20:21:19 -0700

    unsafe practice

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres.awycoggvfjvkoauzbhqw:L5cXxtG1tTXF7Z9W@aws-0-us-east-1.pooler.supabase.com:6543/postgres" #os.environ.get('DATABASE_URL') or os.environ.get('POSTGES_URL')    
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.awycoggvfjvkoauzbhqw:L5cXxtG1tTXF7Z9W@aws-0-us-east-1.pooler.supabase.com:6543/postgres" #os.environ.get('DATABASE_URL') or os.environ.get('POSTGES_URL')    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max upload size
